1a9961d9605a1f99b82d97334b46104ac1152a2e
Teresa <teresa.ambroa@hotmail.com>
2024-08-20 15:33:32 +0200

    install cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { connectDB } = require("./src/utils/db");
+const cors = require("cors");
 const router = require("./src/api/routes/character.routes");
 const env = require("dotenv");
 env.config()
@@ -8,6 +9,7 @@ connectDB();
 const server = express();
 const PORT = process.env.PORT;
 
+server.use(cors())
 server.use(express.json())
 server.use("/", router);
 
